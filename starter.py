ee00ee291246ee0e4ad2d7a7b6736bec11bd29a1
Mwiza MBP 2017 <mwiza@agripredict.com>
2018-10-08 10:38:28 +0200

    Done

---
+++ starter.py
@@ -0,0 +1,10 @@
+from aluLib import *
+
+# Screen width and height
+window_width = 1500
+window_height = 400
+
+def main():
+   pass
+
+start_graphics(main, width=window_width, height=window_height)
